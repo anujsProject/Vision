f3b8c2ca916e06f4879ba007c74e8c8b460530f0
anujsProject <52837557+anujsProject@users.noreply.github.com>
2020-03-03 00:01:10 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,44 @@
+let slideNo = 1;
+let slides = Array.from(document.querySelectorAll(`.header__img-item`));
+slides[0].style.opacity = "1";
+
+const moveSlide = (n) => {
+
+    let current, next;
+    let animateSlides = {
+        forCurrent: '',
+        forNext: ''
+    };
+
+    if(n > slideNo) {
+        if(n > slides.length) n = 1;
+        animateSlides.forCurrent = 'curRightLeft';
+        animateSlides.forNext = 'nextRightLeft';
+    }
+
+    else if(n < slideNo) {
+        if(n == 0) n = slides.length;
+        animateSlides.forCurrent = 'curLeftRight';
+        animateSlides.forNext = 'nextLeftRight';
+    }
+
+    slides.forEach((el, i) => {
+        el.style.opacity = '0';
+        el.className = `header__img-item header__img-item--${i + 1}`;
+    });
+
+    current = slideNo;
+    next = n;
+
+    slides[current - 1].classList.add(animateSlides.forCurrent);
+    slides[next - 1].classList.add(animateSlides.forNext);
+    
+    slideNo = n;
+};
+
+const changeSlide = (plus) => {
+    moveSlide(slideNo + plus);
+};
+
+document.querySelector(`.arr--prev`).addEventListener('click', () => changeSlide(-1));
+document.querySelector(`.arr--next`).addEventListener('click', () => changeSlide(1));
