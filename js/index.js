347684e935f5913aff0ba06f29a1890afedfec0f
anujsProject <52837557+anujsProject@users.noreply.github.com>
2020-06-26 14:40:25 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,48 +1,45 @@
 let slideNo = 1;
 let slides = Array.from(document.querySelectorAll(`.header__img-item`));
-slides[0].style.opacity = "1";
+slides[0].style.visibility = "visible";
 
 const moveSlide = (n) => {
+  let current, next;
+  let animateSlides = {
+    forCurrent: "",
+    forNext: "",
+  };
 
-    let current, next;
-    let animateSlides = {
-        forCurrent: '',
-        forNext: ''
-    };
+  if (n > slideNo) {
+    if (n > slides.length) n = 1;
+    animateSlides.forCurrent = "curRightLeft";
+    animateSlides.forNext = "nextRightLeft";
+  } else if (n < slideNo) {
+    if (n == 0) n = slides.length;
+    animateSlides.forCurrent = "curLeftRight";
+    animateSlides.forNext = "nextLeftRight";
+  }
 
-    if(n > slideNo) {
-        if(n > slides.length) n = 1;
-        animateSlides.forCurrent = 'curRightLeft';
-        animateSlides.forNext = 'nextRightLeft';
-    }
+  slides.forEach((el, i) => {
+    el.style.visibility = "hidden";
+    el.className = `header__img-item header__img-item--${i + 1}`;
+  });
 
-    else if(n < slideNo) {
-        if(n == 0) n = slides.length;
-        animateSlides.forCurrent = 'curLeftRight';
-        animateSlides.forNext = 'nextLeftRight';
-    }
+  current = slideNo;
+  next = n;
 
-    slides.forEach((el, i) => {
-        el.style.opacity = '0';
-        el.style.visibility = 'hidden';
-        el.className = `header__img-item header__img-item--${i + 1}`;
-    });
-    
-    slides[slideNo - 1].style.opacity = '1';
-    slides[slideNo - 1].style.visibility = 'visible';
+  slides[current - 1].classList.add(animateSlides.forCurrent);
+  slides[next - 1].classList.add(animateSlides.forNext);
 
-    current = slideNo;
-    next = n;
-
-    slides[current - 1].classList.add(animateSlides.forCurrent);
-    slides[next - 1].classList.add(animateSlides.forNext);
-    
-    slideNo = n;
+  slideNo = n;
 };
 
 const changeSlide = (plus) => {
-    moveSlide(slideNo + plus);
+  moveSlide(slideNo + plus);
 };
 
-document.querySelector(`.arr--prev`).addEventListener('click', () => changeSlide(-1));
-document.querySelector(`.arr--next`).addEventListener('click', () => changeSlide(1));
+document
+  .querySelector(`.arr--prev`)
+  .addEventListener("click", () => changeSlide(-1));
+document
+  .querySelector(`.arr--next`)
+  .addEventListener("click", () => changeSlide(1));
